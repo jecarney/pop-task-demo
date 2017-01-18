6781b5146482bef824714f9f37288a4ff673177e
jecarney <julesaccts@gmail.com>
2017-01-17 21:54:08 -0500

    updated css

--- components/demo_screen.js
+++ components/demo_screen.js
@@ -3,12 +3,10 @@ import React, { Component } from 'react';
 class DemoScreen extends Component {
   render() {
         return (
-          <div id="wrapper">
             <div id="demo-wrap">
               {this.props.children}
               <button className="btn" onClick={this.props.next}>Next</button>
             </div>
-          </div>
             );
         }
       }
