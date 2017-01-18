2df997bffb42952062268340870b9b4ba7d762ba
jecarney <julesaccts@gmail.com>
2017-01-17 21:30:37 -0500

    Updated demo text

--- components/current.js
+++ components/current.js
@@ -42,8 +42,8 @@ class Current extends Component {
         <div id="demo-wrapper">
           <DemoScreen next={this.next2}>
             <div className="demo-1">
-              <p>The main screen is for tasks that you are working on today. To start a new task, enter details as desired and click "blow bubble". Click the bubble when you begin working on that task to start timing - the bubble will wobble. Click again to stop timing, or click another bubble to start timing a different task. </p>
-              <p>You do not need to enter any information to create a bubble and start timing - you can simply edit the bubble later. To edit, hover over a bubble and click "Edit". </p>
+              <p>The main screen is for tasks that you are working on today. To start a new task, enter details as desired and click "blow bubble". </p>
+              <p>Once you have a bubble, you can edit or delete, or start timing your progress on the given task.</p>
             </div>
           </DemoScreen>
           <App>
@@ -57,7 +57,7 @@ class Current extends Component {
           <DemoScreen next={this.next3}>
             <div className="demo-1">
               <p>Once finished, click "pop" to complete a task. You can click "Popped" to view a record of all completed tasks. </p>
-              <p> To take note of a task to be completed another day, click "Backlog" to go to the backlog screen. You can create new bubbles in the backlog, which can later be moved to the Current screen manually, or they can move automatically if you select a due date. </p>
+              <p> To take note of a task to be completed another day, click "Backlog" to go to the backlog screen. Bubbles in the backlog can be moved to the Current screen manually, or they can move automatically if you select a due date. </p>
               <p> Thanks and enjoy!</p>
             </div>
           </DemoScreen>
